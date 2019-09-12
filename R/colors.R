a15163b66278b3af41b822ca87e4ba74b94445a5
an-bui <a.thanh.bui@gmail.com>
2019-09-12 09:54:24 -0700

    added colors

--- R/colors.R
+++ R/colors.R
@@ -4,13 +4,27 @@
 #'
 #' @export
 cal_palettes <- list(
-  name_1 = c("#A42820", "#5F5647", "#9B110E", "#3F5151", "#4E2A1E", "#550307", "#0C1707"),
-  name_2 = c("#FAD510", "#CB2314", "#273046", "#354823", "#1E1E1E"),
-  name_3 = c("#E1BD6D", "#EABE94", "#0B775E", "#35274A" ,"#F2300F"),
-  name_4 = c("#E1BD6D", "#EABE94", "#0B775E", "#35274A" ,"#F2300F"),
-  name_5 = c("#899DA4", "#C93312", "#FAEFD1", "#DC863B"),
-  name_6 = c("#9A8822", "#F5CDB4", "#F8AFA8", "#FDDDA0", "#74A089"),
-  name_7 = c("#3B9AB2", "#78B7C5", "#EBCC2A", "#E1AF00", "#F21A00")
+  sierra1 = c("#BD973D", "#3B7D6E", "#5F5C29", "#262E43", "#4D5B75", "#5792CC"),
+  sierra2 = c("#4CA2B0", "#FDD989", "#516238", "#5A8B92", "#395B5F"),
+  chaparral1 = c("#DCC27A", "#B0B9BE", "#63605F", "#985E5C", "#AEBFA8", "#F19B34"),
+  chaparral2 = c("#9A9B5F", "#D9E4DC", "#D98A63", "#A7C2CD", "#C5D2D2"),
+  chaparral3 = c("#D3E3CA", "#BED6B3", "#92A587", "#2F3525", "#4A5438"),
+  conifer = c("#CC7540", "#765043", "#A69260", "#979A6B", "#9A9657"),
+  desert = c("#F6EECF", "#ECD6AB", "#B09175", "#632D1F", "#291611"),
+  wetland = c("#3F320D", "#AD6F4F", "#DED4C8", "#2B3851", "#AEC96F"),
+  oak1 = c("#EFC68E", "#B58755", "#7C9867", "#4F5730","#7A5028"),
+  oak2 = c("#ACB870", "#59672A", "#D7D3DA", "#665543", "#262722"),
+  kelp1 = c("#114C54", "#009BB0", "#54662C", "#BE8333", "#FFBF00","#C70000"),
+  kelp2 = c("#6D5A18", "#3D6334", "#368000", "#026779", "#0FB2D3"),
+  coastaldune1 = c("#DCC8BA", "#DCD6C5", "#B4AA98", "#D7DCE4", "#444239"),
+  coastaldune2 = c("#E2D78A", "#E4B3E2", "#372E21", "#90816E", "#523833"),
+  superbloom1 = c("#ECAB99", "#F1C100", "#9484B1", "#5B6530", "#B9C7E2"),
+  superbloom2 = c("#DE7424", "#F5CA37", "#AD8D26", "#496849", "#654783"),
+  superbloom3 = c("#E69512", "#D3105C", "#3B4F8E", "#3A5D3D", "#4C4976", "#6C91BD"),
+  sbchannel = c("#A1CAF6", "#6592D6", "#1E2F46", "#375377", "#4C6FA1"),
+  lake = c("#3793EC", "#7AC9B7", "#CECEB9", "#2A3927", "#6CA184"),
+  fire = c("#B77B7B", "#FEEC44", "#F66C09", "#E60505", "#2C1B21"),
+  agriculture = c("#A45C44", "#5A7F3C", "#CACA91", "#2C3B26", "#88B063")
 )
 
 #' A California ecosystem color palette generator
@@ -32,15 +46,15 @@ cal_palettes <- list(
 #' @export
 #' @keywords colors
 #' @examples
-#' cal_palette("name_1")
-#' cal_palette("name_2")
-#' cal_palette("name_3")
-#' cal_palette("name_4", 3)
+#' cal_palette("chaparral1")
+#' cal_palette("chaparral2")
+#' cal_palette("chaparral3")
+#' cal_palette("coastaldune1", 3)
 #'
 #' # If you need more colours than normally found in a palette, you
 #' # can use a continuous palette to interpolate between existing
 #' # colours
-#' pal <- cal_palette(21, name = "name_1", type = "continuous")
+#' pal <- cal_palette(21, name = "coastaldune1", type = "continuous")
 #' image(volcano, col = pal)
 #'
 
